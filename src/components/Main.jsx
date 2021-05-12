79f8273272138b8d8e1bcc4c16b5f1ced68b4d7c
egray <egray@qa.com>
2021-05-12 16:08:41 +0100

    second profile

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Nav from './Navbar'
 import Container from 'react-bootstrap/Container'
 import Profile from './Profile'
@@ -6,10 +6,15 @@ import ProfileContent from './ProfileContent'
 
 
 const Main = () => {
+
+
+
     return (
+        
         <>
         <Nav />
         <Container></Container>
+        <h1>First Name, Last Name</h1>
         <ProfileContent/>
         <Profile/>
         </>
--- src/components/ProfileContent.js
+++ src/components/ProfileContent.js
@@ -1,70 +1,143 @@
 import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import Typography from '@material-ui/core/Typography';
 
-class ProfileContent extends Component {
-  render () {
-    return (
-<div>
-  <title>Profile</title>
+const useStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+    },
+    heading: {
+      fontSize: theme.typography.pxToRem(15),
+      fontWeight: theme.typography.fontWeightRegular,
+    },
+  }));
 
-<body>
+export default function SimpleAccordion() {
+  const classes = useStyles();
 
-  
-<div class="container-fluid text-center">    
-  <div class="row content">
-    <div class="col-sm-8 text-left"> 
-
-      <h1>First Name, Last Name</h1>
-    
-     <h3>Date of Birth</h3>
-      <p>0000/00/00</p>
-
-      <h3>Address</h3>
-           666 Low Street <br/>
-            Manchester <br/>
-           MA8 2FE <br/>
-
-      <h3>Driver Licence ID</h3>
-      <p>ROBIO123123KL9K123</p>
-
-      <h3>Vehicle Registration No.</h3>
-      <p>FK59 TDY</p>
-
-      <h3>Vehicle Details</h3>
-      <h5>      
-
-             Make: Ford <br/>
-            Model: Punto <br/>
-            Colour: Yellow <br/>
-            Registration Date: <br/>
+  return (
+    <div className={classes.root}>
+      <Accordion>
+        <AccordionSummary>
+          <Typography className={classes.heading}>Date of Birth</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            09/05/1994
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
       
-            </h5>
+      <Accordion>
+        <AccordionSummary>
+          <Typography className={classes.heading}>Address</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+                666 Low Street <br/>
+                Manchester <br/>
+                MA8 2FE <br/>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
 
-</div>
-  </div>
-</div>
+      <Accordion>
+        <AccordionSummary>
+          <Typography className={classes.heading}>Driver Licence ID</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          ROBIO123123KL9K123
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
 
-{/* /* "registrationID": "112111",
-    "registrationDate": "2002-11-11",
-    "vehicleRegistrationNo": "FK59 TDY",
-    "make": "Ford",
-    "model": "Punto",
-    "colour": "Yellow",
-    "forename": "Karen",
-    "surname": "Robertson",
-    "address": {
-        "streetAddress" "666 Low Street",
-        "city": "Manchester",
-        "postalCode": "MT11 5DD"
+      <Accordion>
+        <AccordionSummary>
+          <Typography className={classes.heading}>Vehicle Registration No.</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+          FK59 TDY
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
 
-    "dateOfBirth": "1975-02-06",
-    "driverLicenceID": "ROBIO123123KL9K123" */ }
+      <Accordion>
+        <AccordionSummary>
+          <Typography className={classes.heading}>Vehicle Details</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+               Make: Ford <br/>
+               Model: Punto <br/>
+               Colour: Yellow <br/>
+               Registration Date: <br/>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
+}
 
 
 
-</body>
-</div>
+// class ProfileContent extends Component {
+//   render () {
+//     return (
+// <div>
+//   <title>Profile</title>
 
-    );
-}}
+// <body>
 
-export default ProfileContent;
+// <ul class="text">FIRST NAME, LAST NAME</ul>
+    
+// const { title, content } = accordionData;
+
+// <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+//   <div class="panel panel-default">
+//     <div class="panel-heading " role="tab" id="headingOne">
+//       <h4 class="panel-title ">
+//         <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="" aria-controls="collapseOne">
+//           <div class="title  btn btn-danger btn-outline btn-lg">DATE OF BIRTH</div>
+//         </a>
+//       </h4>
+//     </div>
+//     <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+//       <div class="panel-body">
+//         06/</div>
+//       </div>
+//     </div>
+//   </div>
+
+//       <h3>Address</h3>
+//            666 Low Street <br/>
+//             Manchester <br/>
+//            MA8 2FE <br/>
+
+//       <h3>Driver Licence ID</h3>
+//       <p>ROBIO123123KL9K123</p>
+
+//       <h3>Vehicle Registration No.</h3>
+//       <p>FK59 TDY</p>
+
+//       <h3>Vehicle Details</h3>
+//       <h5>      
+
+//              Make: Ford <br/>
+//             Model: Punto <br/>
+//             Colour: Yellow <br/>
+//             Registration Date: <br/>
+      
+//             </h5>
+
+// </body>
+// </div>
+
+//     );
+// }}
+
+// export default ProfileContent;
