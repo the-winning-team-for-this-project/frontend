6db7a98dea06d6fadc93573eefde0f59ba7282b1
CP11B <CPrestwich@academytrainee.com>
2021-05-14 10:57:34 +0100

    bootstrap

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react'
 import Nav from './Navbar'
 import Container from 'react-bootstrap/Container'
 import Profile from './Profile'
-import ProfileContent from './ProfileContent'
 
 
 const Main = () => {
@@ -14,8 +13,7 @@ const Main = () => {
         <>
         <Nav />
         <Container></Container>
-        <h1>First Name, Last Name</h1>
-        <ProfileContent/>
+
         <Profile/>
         </>
     )
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import { Accordion, Card, Button } from 'react-bootstrap'
+import axios from 'axios'
+ 
 
-function Profile() {
-  return (
-    <></>
+  const searchProfiles = (e) => {
+    axios.get(`http://localhost:5000/vehicle`)
+    .then(response => {
+      console.log(response.data);
+    });
+  };
+
+
+
+  const Profile = () => {
+    return (
+    <div>
+      <Accordion defaultActiveKey="0">
+      <Card>
+        <Card.Header>
+          <Accordion.Toggle as={Button} variant="link" eventKey="0">
+            Vehicle Information
+          </Accordion.Toggle>
+        </Card.Header>
+        <Accordion.Collapse eventKey="0">
+          <Card.Body>Hello! I'm the body</Card.Body>
+        </Accordion.Collapse>
+      </Card>
+      </Accordion>
+
+    </div>
   );
 }
 
-export default Profile;
+export default Profile
