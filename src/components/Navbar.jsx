7786d136fb34a27125ecd85aa20e08f4aa7b1cb4
CP11B <CPrestwich@academytrainee.com>
2021-05-14 15:46:50 +0100

    fully working front end

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
-import Navbar from 'react-bootstrap/Navbar'
+import {Navbar, Nav, NavDropdown} from 'react-bootstrap'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import Container from 'react-bootstrap/Container'
+import FetchProfile from './FetchProfile'
+import RegSearch from './RegSearch'
 
-const Nav = () => {
-    // vehicle-search
-    // getSuspect/<reg number>
+const SiteNavbar = () => {
+
+
     return (
         <>
-            <Navbar className="navbar" bg="dark" variant="dark">
-            <Navbar.Brand>Final Project</Navbar.Brand>
-            </Navbar>
 
+<Navbar className="navbar" bg="dark" variant="dark">
+
+  <Navbar.Brand href="">Final Project</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/vehicle-search">Vehicle Search</Nav.Link>
+
+    </Nav>
+
+  </Navbar.Collapse>
+</Navbar>
             
         </>
     )
 }
 
-export default Nav
+export default SiteNavbar
