5c70996d338fdddcbe4e3951fbd5c131633a0c53
CP11B <CPrestwich@academytrainee.com>
2021-05-17 21:52:43 +0100

    quick bug fix

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Container from 'react-bootstrap/Container'
 import FetchProfile from './FetchProfile'
 import RegSearch from './RegSearch'
