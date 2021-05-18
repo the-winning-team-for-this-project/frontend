4da80fa801bca867b2c501d4df9a985f5796e876
CP11B <CPrestwich@academytrainee.com>
2021-05-18 10:29:10 +0100

    name form added, quick map tidy up

---
+++ src/components/NameSearch.jsx
@@ -0,0 +1,48 @@
+import { Form, Button, Jumbotron, Col } from 'react-bootstrap'
+import { useHistory } from 'react-router-dom'
+import { useState } from 'react'
+
+const NameSearch = () => {
+
+    const [forenameSearch, setForenameSearch] = useState("")
+    const [surnameSearch, setSurnameSearch] = useState("")
+    const history = useHistory()
+
+    const handleFNSubmit = (e) => {
+        e.preventDefault()
+        setForenameSearch(e.target.value) 
+    }
+
+    const handleSNSubmit = (e) => {
+        e.preventDefault()
+        setSurnameSearch(e.target.value) 
+    }
+
+    const handleClick = () => {
+        history.push("/getSuspect/" + forenameSearch + " " + surnameSearch)
+    }
+
+    return (
+        <>
+        <Jumbotron>
+        <h1>Person Search</h1>
+        <p>
+          Search for a person's name.
+        </p>
+        <Form>
+            <Form.Row>
+                <Col>
+                    <Form.Control required size="lg" type="text" placeholder="Enter forename here" value={forenameSearch} onChange={(e)=>handleFNSubmit(e)}/>
+                </Col>
+                <Col>               
+                    <Form.Control required size="lg" type="text" placeholder="Enter surname here" value={surnameSearch} onChange={(e)=>handleSNSubmit(e)}/>
+                </Col>
+            </Form.Row>
+            <Button type="button" onClick={handleClick}>Submit</Button>
+        </Form>  
+        </Jumbotron>       
+        </>
+    )
+}
+
+export default NameSearch
