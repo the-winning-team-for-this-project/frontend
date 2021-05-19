b6ededee3a184b84d6bc8f963e0a00c8ba41ab5d
egray <egray@qa.com>
2021-05-19 13:54:21 +0100

    text change

--- src/components/RegSearch.jsx
+++ src/components/RegSearch.jsx
@@ -21,7 +21,7 @@ const RegSearch = () => {
         <Jumbotron>
         <h1>Vehicle Search</h1>
         <p>
-          Search for a vehicle using a registration plate
+          Search for vehicle using registration plate
         </p>
         <Form>
             <Form.Group>
