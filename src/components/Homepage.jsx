a8460e6bc4dc01edb127d03ed48b12b2786c252b
egray <egray@qa.com>
2021-05-20 14:47:07 +0100

    homepage update

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,5 +1,3 @@
-import { Container } from 'react-bootstrap'
-import { Jumbotron } from 'react-bootstrap'
 import Aos from 'aos';
 import "aos/dist/aos.css";
 import React, { useEffect } from 'react';
@@ -10,40 +8,6 @@ const Homepage = () => {
         Aos.init({ duration: 2000 });
     }, []);
     return (
-//         <Container className='homecontainer'>
-//         <Jumbotron fluid className='jumone'>
-//   <Container>
-//   <h1><strong>Search for Person Information</strong></h1>
-//         <br />
-//         <h3>
-//                 Direct to search using the Navigation Bar at the top of this page. 
-//             <br />
-//             <br />
-//             Input known information.
-//             <br />
-//             <br />
-//             You will then be redirected to a page containing further information on that person.
-            
-//         </h3>
-//   </Container>
-// </Jumbotron>
-//         <Jumbotron fluid className='jumtwo'>
-//   <Container>
-//     <h1>Hello World</h1>
-//     <p>
-//       Some things and words and things in here.
-//     </p>
-//   </Container>
-// </Jumbotron>
-// <Jumbotron fluid className='jumthree'>
-//   <Container>
-//     <h1>Hello World</h1>
-//     <p>
-//       More words here.
-//     </p>
-//   </Container>
-// </Jumbotron>
-//         </Container>
 
 <div className="Boxy">
     <div className="grids">
@@ -53,8 +17,8 @@ const Homepage = () => {
                 Hello
                  </h1>
         </div>
-        <div data-aos="flip-left" className="boxes" className="b1">
-     <h3>
+        <div data-aos="fade-up" className="boxes" className="b1">
+     <h4>
      <br />
             <br />
         Direct to search using the Navigation Bar at the top of this page. 
@@ -65,9 +29,9 @@ const Homepage = () => {
              <br />
              You will then be redirected to a page containing further information on that person.
             
-         </h3>
+         </h4>
         </div>
-        <div data-aos="fade-up" className="boxes" className="b2">info</div>
+        <div data-aos="fade-up" className="boxes" className="b2"></div>
         <div data-aos="fade-left" className="boxes" className="b3">info</div>
         <div data-aos="fade-right" className="boxes" className="b4">info</div>
     </div>
