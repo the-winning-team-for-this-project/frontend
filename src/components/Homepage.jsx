e5c8b2e64fc04beb8c26ff2a3dbcb4fb96e924e3
egray <egray@qa.com>
2021-05-20 10:49:31 +0100

    added jumbotrons to homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,9 +1,12 @@
 import { Container } from 'react-bootstrap'
+import { Jumbotron } from 'react-bootstrap'
 
 const Homepage = () => {
     return (
         <Container className='homecontainer'>
-        <h1><strong>Search for Person Information</strong></h1>
+        <Jumbotron fluid className='jumone'>
+  <Container>
+  <h1><strong>Search for Person Information</strong></h1>
         <br />
         <h3>
                 Direct to search using the Navigation Bar at the top of this page. 
@@ -15,6 +18,24 @@ const Homepage = () => {
             You will then be redirected to a page containing further information on that person.
             
         </h3>
+  </Container>
+</Jumbotron>
+        <Jumbotron fluid className='jumtwo'>
+  <Container>
+    <h1>Hello World</h1>
+    <p>
+      Some things and words and things in here.
+    </p>
+  </Container>
+</Jumbotron>
+<Jumbotron fluid className='jumthree'>
+  <Container>
+    <h1>Hello World</h1>
+    <p>
+      More words here.
+    </p>
+  </Container>
+</Jumbotron>
         </Container>
         
         
