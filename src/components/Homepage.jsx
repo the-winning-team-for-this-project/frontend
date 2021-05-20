476327fafc2d3a7449d05bd4741028946f9d794e
egray <egray@qa.com>
2021-05-20 15:33:30 +0100

    homepage update

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -14,26 +14,52 @@ const Homepage = () => {
         <div className="boxes">
             <h1>
                 <br />
-                Hello
+                Welcome
                  </h1>
+                 <br />
+                 <h3>
+                     This application can be used to search for person's of interest using name or vehicle registration details.
+                 </h3>
         </div>
-        <div data-aos="fade-up" className="boxes" className="b1">
+        <div className="boxes" className="b1">
      <h4>
      <br />
-            <br />
-        Direct to search using the Navigation Bar at the top of this page. 
+     <br />
+        How to use:
              <br />
             <br />
-             Input known information.
+        Direct to vehicle or name search using the Navigation Bar at the top of this page 
+             <br />
+            <br />
+             When redirected, input known information and submit
              <br />
              <br />
-             You will then be redirected to a page containing further information on that person.
+             You will then be redirected to a page containing further information on that person
             
          </h4>
         </div>
-        <div data-aos="fade-up" className="boxes" className="b2"></div>
-        <div data-aos="fade-left" className="boxes" className="b3">info</div>
-        <div data-aos="fade-right" className="boxes" className="b4">info</div>
+        <div data-aos="fade-up" className="boxes" className="b2">
+            <h4>
+            <br />
+                <br />
+                Application created by:
+                <br />
+                <br />
+                Cara Prestwich
+                <br />
+                Junaid Sidat 
+                <br />
+                Joseph Woodward
+                <br />
+                Emily Nixon
+                <br />
+                Emma Gray 
+                <br />
+                <br />
+                NSAC 2021
+            </h4>
+        </div>
+
     </div>
 </div>
         
